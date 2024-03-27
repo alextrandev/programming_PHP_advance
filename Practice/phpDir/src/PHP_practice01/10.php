726b802392a37993ed755d4c9b83812121b7af26
dk10119 <tranduclong092@gmail.com>
2024-03-27 23:29:09 +0200

    complete object practice

--- Practice/phpDir/src/PHP_practice01/10.php
+++ Practice/phpDir/src/PHP_practice01/10.php
@@ -12,31 +12,36 @@
 
   <article class="main-content col-xs-8">
 
-
-    <?php
-
+  <?php
 		/*  Step 1: Use the Make a class called Dog
-
 		Step 2: Set some properties for Dog, Example, eye colors, nose, or fur color
-
 		Step 4: Make a method named ShowAll that echos all the properties
-
 		Step 5: Instantiate the class / create object and call it pitbull
-
-Step 6: Call the method ShowAll
-
-	
-
-		
+    Step 6: Call the method ShowAll
 	*/
+  class Dog {
+    public $name, $eyeColor, $nose, $furColor;
 
-		?>
+    function __construct($name, $eyeColor, $nose, $furColor) {
+      $this->name = $name;
+      $this->eyeColor = $eyeColor;
+      $this->nose = $nose;
+      $this->furColor = $furColor;
+    }
 
+    function showAll() {
+      echo "$this->name, eye color: $this->eyeColor, nose: $this->nose, fur color: $this->furColor.";
+    }
+  }
 
+  $pitbull = new Dog("Pitbull", "brown", "round","white");
 
+  $pitbull->showAll();
 
-
+	?>
+    
   </article>
+</section>
   <!--MAIN CONTENT-->
 
   <?php include "includes/footer.php"; ?>
--- Practice/phpDir/src/PHP_practice06/oop/section_a/c04/constructor-methods.php
+++ Practice/phpDir/src/PHP_practice06/oop/section_a/c04/constructor-methods.php
@@ -1,59 +1,70 @@
 <?php
-/* Write your PHP code here
-
+/*
 Step 1: Use and declare php strict types
-
 Step 2:  Create a class for account (with its properties e.g. number, type and balance)
-
 Step 3: Create a PHP constructor with argument types e.g. number, type and balance
 If a balance is not given when creating the object, a default value of 0.00 should be used.
-
 Step 4: Create two methods for deposit and withdraw which will update the value
 stored in the balance property. They should be given arguments and return type declaration of float.
-
 Step 5: Create two objects to represent a checking account and savings account. 
-
 Step 6:  An HTML skeleton is drawn. The first row should show headings using 
 the type property of two objects. To access property use the:
  - Name of the variable that holds the object
  - Object operator
  - Property name
-
 Step 7: In the next table row show the balance property of the objects.
-
 Step 8: In the third row of the table, the balance of each account is updated by calling deposit() or
 withdraw() methods. These methods return the new value of balance property and this should be shown in page.
 To call a method, you can use
 - Name of the variable that holds the object
 - Object operator
 - Method name with its argument in parantheses
-
 Step 9: In the final row of the table, previous step is repeated using different values. 
-
-
-
 */
 
-?>
+declare(strict_types=1); ?>
 
-<?php include 'includes/header.php'; ?>
+<?php
+$accounts = [];
+
+class Account {
+  public $number, $type, $balance;
+
+  function __construct(int $number, string $type, float $balance) {
+    $this->number = $number;
+    $this->type = $type;
+    $this->balance = $balance;
+  }
+
+  function displayRow() {
+    echo <<<HTML
+    <tr>
+      <td>$this->number</td>
+      <td>$this->type</td>
+      <td>$this->balance</td>
+    </tr>
+    HTML;
+  }
+}
+
+$accounts[] = new Account(32002928172, "savings", 5123.06);
+$accounts[] = new Account(32001924827, "investing", 9992.01);
+$accounts[] = new Account(32003105818, "savings", 2232.12);
+
+include 'includes/header.php'; ?>
+
 <h2>Account Balances</h2>
 <table>
   <tr>
-    <th>Date</th>
-
+    <th>Number</th>
+    <th>Type</th>
+    <th>Balance</th>
   </tr>
-  <tr>
-    <td>23 June</td>
 
-  </tr>
-  <tr>
-    <td>24 June</td>
+  <?php foreach ($accounts as $account) {
+    $account->displayRow();
+  } ?>
 
-  </tr>
-  <tr>
-    <td>25 June</td>
-
-  </tr>
 </table>
+
 <?php include 'includes/footer.php'; ?>
