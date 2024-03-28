52c8b1df04e0af64e0a2b215899f42ccb61e8180
dk10119 <tranduclong092@gmail.com>
2024-03-28 22:53:53 +0200

    add fetch data from database and display

---
+++ Practice/phpDir/src/CRUDApp/displayDB.php
@@ -0,0 +1,30 @@
+<?php
+$query = "SELECT * FROM users";
+$result = mysqli_query($conn, $query);
+
+if ($result) : ?>
+
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Password</th>
+        </tr>
+
+        <?php while ($rows = mysqli_fetch_assoc($result)) : ?>
+
+            <tr>
+                <?php foreach ($rows as $row) : ?>
+                    <td><?= $row ?></td>
+                <?php endforeach; ?>
+            </tr>
+
+        <?php endwhile; ?>
+
+    </table>
+
+<?php else :
+
+    die("Query insertation failed");
+
+endif; ?>
