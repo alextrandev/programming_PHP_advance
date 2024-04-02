2ae87d37d038a300aa1ca9e2db1245903511b54d
dk10119 <tranduclong092@gmail.com>
2024-04-02 13:02:37 +0300

    move username and password check function into a new file

--- Practice/phpDir/src/CRUDApp/login.php
+++ Practice/phpDir/src/CRUDApp/login.php
@@ -8,36 +8,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $user = htmlspecialchars($_POST["username"]);
         $pwd = htmlspecialchars($_POST["password"]);
 
-        switch (false) {
-            case strlen($user) >= 4:
-                $error_msg = "Username must have at least 4 characters";
-                break;
-            case strlen($pwd) >= 8:
-                $error_msg = "Password must have at least 8 characters";
-                break;
-            case preg_match("~[A-ZÅÄÖ]~", $pwd):
-                $error_msg = "Password must contain an uppercase";
-                break;
-            case preg_match("~[a-zäåö]~", $pwd):
-                $error_msg = "Password must contain a lowercase";
-                break;
-            case preg_match("~[1-9]~", $pwd):
-                $error_msg = "Password must contain a number";
-                break;
-            default:
-                $error_msg = "";
-                //create the records in the database
-                if ($stmt->execute()) {
-                    header("Location: " . $_SERVER["PHP_SELF"]);
-                    exit();
-                } else die("Query insertion failed");
-        }
+        include "input_check.php";
+
         $stmt->close();
         $conn->close();
     };
-};
-
-?>
+} ?>
 
 <form action="login.php" method="post">
     <label for="username">Username</label>
--- Practice/phpDir/src/CRUDApp/update.php
+++ Practice/phpDir/src/CRUDApp/update.php
@@ -7,35 +7,11 @@
     $pwd = htmlspecialchars($_POST["password"]);
     $id = htmlspecialchars($_POST["id"]);
 
-    switch (false) {
-        case strlen($user) >= 4:
-            $error_msg = "Username must have at least 4 characters";
-            break;
-        case strlen($pwd) >= 8:
-            $error_msg = "Password must have at least 8 characters";
-            break;
-        case preg_match("~[A-ZÅÄÖ]~", $pwd):
-            $error_msg = "Password must contain an uppercase";
-            break;
-        case preg_match("~[a-zäåö]~", $pwd):
-            $error_msg = "Password must contain a lowercase";
-            break;
-        case preg_match("~[1-9]~", $pwd):
-            $error_msg = "Password must contain a number";
-            break;
-        default:
-            $error_msg = "";
-            //create the records in the database
-            if ($stmt->execute()) {
-                header("Location: " . $_SERVER["PHP_SELF"]);
-                exit();
-            } else die("Query insertion failed");
-    }
+    include "input_check.php";
 
     $stmt->close();
     $conn->close();
-}
-?>
+} ?>
 
 <form action="update.php" method="post">
     <label for="username"> Username </label>
