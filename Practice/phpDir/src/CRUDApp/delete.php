f680d81950e4b6018d4e83810906b2e94c7f80d9
dk10119 <tranduclong092@gmail.com>
2024-04-03 11:56:20 +0300

    add confirmation when deleting user

--- Practice/phpDir/src/CRUDApp/delete.php
+++ Practice/phpDir/src/CRUDApp/delete.php
@@ -1,4 +1,26 @@
-<?php if (isset($_GET["id"])) :
+<?php if (isset($_GET["id"]) && !isset($_GET["confirm"])) :
+
+    include "db.php";
+
+    $id = htmlspecialchars($_GET["id"]);
+    $query = "SELECT * FROM users WHERE id=$id";
+    $result = mysqli_query($conn, $query);
+    $user = mysqli_fetch_assoc($result);
+?>
+    <h1>Delete user <?= $user["id"] . ": " . $user["username"]  ?></h1>
+    <form action="delete.php?id=<?= $user["id"] ?>&confirm=1" method="post">
+        <label for="username"> Username </label>
+        <input type="text" name="username" value="<?= $user["username"] ?>" readonly><br>
+        <label for="password"> Password </label>
+        <input type="password" name="password" value="<?= $user["password"] ?>" readonly><br>
+        <label for="id">User ID</label>
+        <input type="number" name="id" value="<?= $user["id"] ?>" readonly><br>
+        <input onclick="location.href='delete.php?id=<?= $user['id'] ?>&confirm=1'" class=" button" type="submit" value="DELETE">
+        <input type="button" onclick="history.go(-1)" value="Back to index page">
+    </form>
+<?php
+
+elseif (isset($_GET["confirm"])) :
 
     include "db.php";
     $id = htmlspecialchars($_GET["id"]) ?? 0;
@@ -16,3 +38,8 @@ else :
     exit();
 
 endif;
+$conn->close();
+
+?>
+
+<link rel="stylesheet" href="style.css">
