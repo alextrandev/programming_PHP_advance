a8b1167da9bc59df61f897cc383a0f50f17a86f0
dk10119 <tranduclong092@gmail.com>
2024-04-10 12:22:28 +0300

    sugarcoating

--- Practice/phpDir/src/api/index.php
+++ Practice/phpDir/src/api/index.php
@@ -11,27 +11,25 @@ $connection = $objectDB->connect();
 // var_dump($connection);
 
 // print_r(file_get_contents('php://input'));
-// $user = file_get_contents('php://input');
-$method = $_SERVER['REQUEST_METHOD'];
 
+$method = $_SERVER['REQUEST_METHOD'];
 switch ($method) {
     case "POST":
         $user = json_decode(file_get_contents('php://input'));
-        $sql = "INSERT INTO users(name, email, phone, created_at, updated_at) VALUES(:name, :email, :phone, :created_at, :updated_at)";
+        $sql = "INSERT INTO users(name, email, phone, created_at, updated_at) VALUES(?,?,?,?,?)";
         $stmt = $connection->prepare($sql);
-        $created_at = date('Y-m-d');
-        $updated_at = date('Y-m-d');
-        $stmt->bindParam(':name', $user->name);
-        $stmt->bindParam(':email', $user->email);
-        $stmt->bindParam(':phone', $user->phone);
-        $stmt->bindParam(':created_at', $created_at);
-        $stmt->bindParam(':updated_at', $updated_at);
+        $paramArr = array($user->name, $user->email, $user->phone, date('Y-m-d'), date('Y-m-d'));
+
+        foreach ($paramArr as $key => $val) {
+            $stmt->bindParam($key + 1, $paramArr[$key]);
+        }
 
         if ($stmt->execute()) {
             $res = ['status' => 1, 'message' => 'Record created successfully'];
         } else {
             $res = ['status' => 0, 'message' => 'Failed to create a record'];
         }
+
         echo json_encode($res);
         break;
 }
